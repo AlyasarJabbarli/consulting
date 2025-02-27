521099a174086f01eaed7d25f36494b868fd5980
AlyasarJabbarli <alyasarmj@code.edu.az>
2025-02-27 23:45:06 +0400

    folder structure and package configurations done

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to our consulting company. We provide top-notch services to help your business grow.</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Services.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Services: React.FC = () => {
+    return (
+        <div>
+            <h1>Our Services</h1>
+            <p>Welcome to our services page. Here you can find a list of the services we offer.</p>
+            {/* Add more content and components as needed */}
+        </div>
+    );
+};
+
+export default Services;
